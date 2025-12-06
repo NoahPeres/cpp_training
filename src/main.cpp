4087af81a92323955b74c9e590d0c2677e59c181
Noah Peres <noah.peres@gmail.com>
2025-12-06 14:37:16 +1100

    basic setup

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace yolo
+{
+}
+
+int main(int, char **)
+{
+    int a{5};
+    std::cout << "Hello, from cpp-training!\n";
+    std::cout << "The value of a is: " << a << std::endl;
+
+    return 0;
+}
